eed1dd4e512e7c4cc75dd52d58a9b6c5f39d267a
Kevin Panko <pankok@gmail.com>
2024-04-15 14:54:43 -0400

    Added logging

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import re
 from datetime import date, datetime, timedelta
+import logging
 from typing import List
 from zoneinfo import ZoneInfo
 
@@ -31,18 +32,19 @@ def reschedule_to(task: Task, date: date):
   if task.due.date == date.strftime('%Y-%m-%d'):
     return
 
-  # TODO: use a logging framework?
-  print(f"Sending the task {task.content} to {date}")
+  logging.info(f"Sending the task {task.content} to {date}")
   
   due_date = task.due.string
 
-  due_date = re.sub(r'\bstarting on\b.*', '', due_date)
+  due_date = re.sub(r'\s*starting on.*', '', due_date)
   
   if task.due.is_recurring:
     due_string = f"{due_date} starting on " + date.strftime('%m/%d/%Y')
   else:
     due_string = date.strftime('%m/%d/%Y')
     
+  logging.debug("updating task_id %s with: %s", task.id, due_string)
+
   is_success = api.update_task(task_id=task.id, due_string=due_string)
 
   if not is_success:
@@ -52,7 +54,15 @@ def reschedule_to(task: Task, date: date):
 def slice_list(lst, num_items):
     return lst[:num_items], lst[num_items:]
   
-def schedule_and_push_down(tasks_to_add: List[Task], day: date = None):
+def schedule_and_push_down(
+  tasks_to_add: List[Task],
+  day: date = None,
+  depth: int = 0):
+
+  logging.debug("Called schedule_and_push_down with depth %d", depth)
+  logging.debug("Tasks to add (%d):", len(tasks_to_add))
+  for task in tasks_to_add:
+    logging.debug(task)
 
   if not tasks_to_add:
     return
@@ -62,10 +72,17 @@ def schedule_and_push_down(tasks_to_add: List[Task], day: date = None):
 
   # query all the tasks for this day
 
+  logging.debug("Getting tasks for %s", day)
   tasks = get_tasks_for(day)
+
+  logging.debug("Found %d tasks", len(tasks))
+  for task in tasks:
+    logging.debug(task)
 
   # filter tasks to have only repeating tasks with the '!'
   tasks = [t for t in tasks if t.due.is_recurring and '!' in t.due.string]
+
+  logging.debug("After filter there are now %d tasks", len(tasks))
   
   tasks.extend(tasks_to_add)
   
@@ -76,16 +93,21 @@ def schedule_and_push_down(tasks_to_add: List[Task], day: date = None):
 
   # assign tasks to this day
 
+  logging.debug("%d tasks to reschedule to %s", len(tasks_for_this_day), day)
+
   for task in tasks_for_this_day:
     reschedule_to(task, day)
 
   # recursively call self with rest of the tasks
 
-  schedule_and_push_down(tasks_for_later, day + timedelta(days=+1))
+  schedule_and_push_down(tasks_for_later, day + timedelta(days=+1), depth+1)
   
+
+logging.basicConfig(level=logging.DEBUG)
 
 try:
   # Get all overdue tasks
+  logging.info("Getting overdue tasks")
   overdue_tasks = api.get_tasks(filter='overdue')
 
   schedule_and_push_down(overdue_tasks)
